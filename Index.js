67fdf01164a7de3f9dd4f5768e86fb2ee8c7d03e
Celesxx <debliquy.cameron@gmail.com>
2021-03-31 01:28:06 +0200

    intégration du radar chart dans personnage

--- Index.js
+++ Index.js
@@ -14,6 +14,8 @@ var {_ , shuffle} = require('underscore');
 var fs = require("fs");
 var vm = require('vm');
 const { timeStamp } = require("console");
+var Chart = require('chart.js');
+const ChartJSImage = require('chart.js-image');
 var prefix = config.prefix;
 var prefixbdd = config.prefixbdd;
 var logExist = false
@@ -83,46 +85,70 @@ client.on("message",message =>
     {
         try
         {
-            const text = "Bienvenue il est temps de commencer !"
-            let textFinal = "B"
-            // console.log(`Taille : ${text.length}`)
-            // console.log(`test du text slice : ${text.slice}`)
-            let embed = new Discord.RichEmbed()
-                .setColor("#00FF00")
-                .setAuthor("test dynamique")
-                .setDescription("test")
-                .addField("Test","a")
-            
-                // console.log(embed)
-                // console.log(`la taille de l'array est de ${Object.keys(Array.from(embed.fields)).length}`)
-                message.channel.send(embed).then(async message =>
+            const line_chart = ChartJSImage().chart({
+                "type": "radar",
+                "data": {
+                  "labels": [
+                    "January",
+                    "February",
+                    "March",
+                    "April",
+                    "May",
+                    "June",
+                    "July"
+                  ],
+                  "datasets": [
                     {
-                function edit()
-                {
-                    message.channel.fetchMessages({around: message.id, limit: 1})
-                    .then(msg => {
-                        const fetchedMsg = msg.first();
-                        fetchedMsg.edit(embed);
-                    });
+                      "label": "My First dataset",
+                      "borderColor": "rgb(255,+99,+132)",
+                      "backgroundColor": "rgba(255,+99,+132,+.5)",
+                      "data": [
+                        57,
+                        90,
+                        11,
+                        -15,
+                        37,
+                        -37,
+                        -27
+                      ]
+                    },
+                    {
+                      "label": "My Second dataset",
+                      "borderColor": "rgb(54,+162,+235)",
+                      "backgroundColor": "rgba(54,+162,+235,+.5)",
+                      "data": [
+                        71,
+                        -36,
+                        -94,
+                        78,
+                        98,
+                        65,
+                        -61
+                      ]
+                    }
+                  ]
+                },
+                "options": {
+                  "title": {
+                    "display": true,
+                    "text": "Chart.js Line Chart"
+                  }
                 }
-                
-            for(let i = 1; i < text.length; i++)
-            {
-                    setTimeout(() =>
-                    {
-                        for(const array of Array.from(embed.fields)) 
-                        {
-                                if(array.name == "Test") 
-                                {
-                                    array.value = textFinal
-                                }
-                        }
-                        textFinal += text.slice(i,i+1)
-                        edit()
-                    }, i*1500)
-            }
-            
-        })
+              }) // Line chart
+              .backgroundColor('white')
+              .width(500) // 500px
+              .height(300); // 300px
+
+
+              let InfoPersonnage = new Discord.RichEmbed()
+                .setColor("#bf0000")
+                .setAuthor("test")
+                .setImage(line_chart.toURL())
+            message.channel.send(InfoPersonnage)
+            //   line_chart.toURL(); // String: https://image-charts.com/chart.js/2.8.0?icac=documentation&chart=%7Btype%3A%27line%27%2Cdata%3A%7Blabels%3A%5B%27January%27%2C%27February%27%2C%27March%27%2C%27April%27%2C%27May%27%2C%27June%27%2C%27July%27%5D%2Cdatasets%3A%5B%7Blabel%3A%27My+First+dataset%27%2CborderColor%3A%27rgb%28255%2C+99%2C+132%29%27%2CbackgroundColor%3A%27rgba%28255%2C+99%2C+132%2C+.5%29%27%2Cdata%3A%5B57%2C90%2C11%2C-15%2C37%2C-37%2C-27%5D%7D%2C%7Blabel%3A%27My+Second+dataset%27%2CborderColor%3A%27rgb%2854%2C+162%2C+235%29%27%2CbackgroundColor%3A%27rgba%2854%2C+162%2C+235%2C+.5%29%27%2Cdata%3A%5B71%2C-36%2C-94%2C78%2C98%2C65%2C-61%5D%7D%2C%7Blabel%3A%27My+Third+dataset%27%2CborderColor%3A%27rgb%2875%2C+192%2C+192%29%27%2CbackgroundColor%3A%27rgba%2875%2C+192%2C+192%2C+.5%29%27%2Cdata%3A%5B48%2C-64%2C-61%2C98%2C0%2C-39%2C-70%5D%7D%2C%7Blabel%3A%27My+Third+dataset%27%2CborderColor%3A%27rgb%28255%2C+205%2C+86%29%27%2CbackgroundColor%3A%27rgba%28255%2C+205%2C+86%2C+.5%29%27%2Cdata%3A%5B-58%2C88%2C29%2C44%2C3%2C78%2C-9%5D%7D%5D%7D%2Coptions%3A%7Bresponsive%3Atrue%2Ctitle%3A%7Bdisplay%3Atrue%2Ctext%3A%27Chart.js+Line+Chart+-+Stacked+Area%27%7D%2Ctooltips%3A%7Bmode%3A%27index%27%7D%2Chover%3A%7Bmode%3A%27index%27%7D%2Cscales%3A%7BxAxes%3A%5B%7BscaleLabel%3A%7Bdisplay%3Atrue%2ClabelString%3A%27Month%27%7D%7D%5D%2CyAxes%3A%5B%7Bstacked%3Atrue%2CscaleLabel%3A%7Bdisplay%3Atrue%2ClabelString%3A%27Value%27%7D%7D%5D%7D%7D%7D&bkg=white&width=700&height=390&icretina=1&ichm=922e17b749b1ab7fab2a14cb742029dc46e50e658457913a9f548793910d2a0d
+            //   line_chart.toFile('/path/to/chart.png'); // Promise<()>
+            //   line_chart.toDataURI(); // Promise<String> : data:image/png;base64,iVBORw0KGgo...
+            //   line_chart.toBuffer(); // Promise<Buffer> : Buffer 89 50 4e 47 0d 0a 1a 0a 00 00 00 ...
         }catch(error)
         {
             console.log(error)
